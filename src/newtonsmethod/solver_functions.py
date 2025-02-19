60a484b667e42c4564a18d046cc5f2472b68ded5
akoch23 <akoch23@bu.edu>
2025-02-19 18:08:26 -0500

    Update solver_functions.py

--- src/newtonsmethod/solver_functions.py
+++ src/newtonsmethod/solver_functions.py
@@ -1,17 +1,12 @@
 
-def newton_function_1(x: float):
-    # Example Parabolic Function
-    # Given a float value, will substitute it for variables in below equation and return value. Used to determine the values of f(a), f(b), and f(c) 
-    function_1 = (x)**3 - 4*(x**2) + 1
-    return function_1
 
 
-def newton_function_1_prime(x: float):
-    # Derivative of newton_function_1
-    function_1_prime = 3*(x**2) - 8*(x)
-    return function_1_prime
+def example_SOE_1(x: float):
+    function_1 = x[1]**3 - 4*x[2]**2 + 1
+    function_2 = 3*x[1] - 8*x[2]
+    return example_SOE_1
 
-
+'''
 def newton_function_2(x: float):
     # Example Parabolic Function
     function_2 = (x)**7 - 1000
@@ -59,15 +54,45 @@ def newton_function_4_prime(x: float):
     # Given a float value, will substitute it for variables in below equation and return value. Used to determine the values of f(a), f(b), and f(c) 
    # function_5_prime = 5*(x**4) - 5
    # return function_5_prime
+'''
+
+def compute_residual(x):
+    residuals = np.array(
+    return residuals
+
+def compute_jacobian(x):
+    n = len(x)
+    jacobian = np.zeros((n,n))
+
+    epsilon = 1e-6
+    for i in range(n):
+        x_perturbed = x.copy()
+        x_perturbed[i] += epsilon
+        jacobian[:, i] = (compute_residual(x_perturbed) - compute_residual(x)) / epsilon
+    return jacobian
+
+def newton_method(initial_guess, tolerance=1e-6, max_iter=100):
+    x = initial_guess
+    for iteration in range(max_iter):
+        R = compute_residual(x)
+        J = compute_residual(x)
+
+        try:
+            delta_x = np.lingalg.solve(J, -R)
 
 
+        x_new = x + delta_x
 
-def newton_method(x:float, i:float):
-    for iterations in range(0,i):
-        y = main_function(x)
-        z = prime_function(x)
-        # while f(x_n) > tolerance
-        x_n = x - (y/z)
-        print(f"Iteration {iterations + 1}: x_{iterations + 1} = {x_n}, f(x)= {main_function(x)}")
-        x = x_n
-    return x_n
+        if np.linalg.norm(R) < tolerance:
+            print(f"Convergence reached after {iteration + 1} iterations.")
+            return x_new
+        
+        x = x_new
+
+    print("Max number of iterations reached.")
+    return x
+
+system_of_functions = [example_SOE_1.function_1, example_SOE_1.function_2]
+initial_guess = np.array([1.0, 0.5])
+solution = newtons_method(initial_guess)
+print("Solution: ", solution)
